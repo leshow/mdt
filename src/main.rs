f60e953b73a7009d1616232a71242110ee03094c
Evan Cameron <cameron.evan@gmail.com>
2017-11-28 22:17:42 -0500

    Event iterator

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use pulldown_cmark::{Alignment, Event, Options, Parser, Tag, OPTION_ENABLE_FOOTN
 use std::error::Error;
 use std::fmt;
 use std::io::{self, BufRead, Read};
+use std::borrow::Cow;
 
 fn main() {
     if let Err(e) = run() {
@@ -19,13 +20,36 @@ fn run() -> Result<(), MarkdownError> {
     opts.insert(OPTION_ENABLE_TABLES);
     opts.insert(OPTION_ENABLE_FOOTNOTES);
 
+    // get input
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
 
-    let p = Parser::new_ext(&buffer, opts);
+    // make parser
+    let mut p = Parser::new_ext(&buffer, opts);
+
+    // process events
+    while let Some(event) = p.next() {
+        match event {
+            Event::Start(tag) => start_tag(tag),
+            Event::End(tag) => end_tag(tag),
+            Event::Text(text) => write_text(text),
+            Event::Html(html) | Event::InlineHtml(html) => write_text(html), // don't handle html now
+            Event::SoftBreak => soft_break(),
+            Event::HardBreak => hard_break(),
+            Event::FootnoteReference(name) => footnote(name),
+        }
+    }
     Ok(())
 }
+fn start_tag<'a>(tag: Tag<'a>) {}
 
+fn end_tag<'a>(tag: Tag<'a>) {}
+
+fn write_text<'a>(text: Cow<'a, str>) {}
+
+fn soft_break() {}
+fn hard_break() {}
+fn footnote<'a>(name: Cow<'a, str>) {}
 // Error
 #[derive(Debug)]
 pub(crate) enum MarkdownError {
